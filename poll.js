88bdb7ffcd69754a54aedddf6706e5d36bb1f121
Adam Smith <aceysmith@Adams-Macbook-Pro.local>
2009-10-25 16:13:36 -0700

    update and added jquery

--- poll.js
+++ poll.js
@@ -1,61 +1,62 @@
-
-function assignTitle() 
-{
-  var title = document.getElementById('new_title').value;
-  var state = wave.getState();
-  var delta = {};
-  delta['title'] = title;
-  state.submitDelta(delta);
-}
-function addEntry()
-{
-  var state = wave.getState();
-  var entries = state.get('entries', false);
-  var entryCount = 0;
-  if(entries)
+$(document).ready(function() {
+  $("title_button").click(function()
   {
-    entryCount = entries.length;
-    alert(entryCount);
+    var title = document.getElementById('new_title').value;
+    var state = wave.getState();
+    var delta = {};
+    delta['title'] = title;
+    state.submitDelta(delta);
   }
-  var newEntry = document.getElementById('new_entry').value;
-  var entryName = 'entry' + entryCount;
-  alert(entries);
-  alert(entryName);
-
-  var delta = {};
-  delta['entries'] = { entryName : newEntry };
-  alert(delta['entries']);
-  state.submitDelta(delta);
-}
-
-function render()
-{
-  if(!wave.getState())
+  function addEntry()
   {
-    return;
-  }
-  var state = wave.getState();
-  var title = state.get('title', false);
-  var entries = state.get('entries', false);
-  if(title)
-  {
-    document.getElementById('strowl_poll').innerHTML = title + "<br /><input id='new_entry' size='30'><button onclick='addEntry()'>Add entry</button><br /><div id='entries'></div>";
+    var state = wave.getState();
+    var entries = state.get('entries', false);
+    var entryCount = 0;
     if(entries)
     {
-      for(var i = 0; i < entries.length; i++)
+      entryCount = entries.length;
+      alert(entryCount);
+    }
+    var newEntry = document.getElementById('new_entry').value;
+    var entryName = 'entry' + entryCount;
+    alert(entries);
+    alert(entryName);
+
+    var delta = {};
+    delta['entries'] = { entryName : newEntry };
+    alert(delta['entries']);
+    state.submitDelta(delta);
+  }
+
+  function render()
+  {
+    if(!wave.getState())
+    {
+      return;
+    }
+    var state = wave.getState();
+    var title = state.get('title', false);
+    var entries = state.get('entries', false);
+    if(title)
+    {
+      document.getElementById('strowl_poll').innerHTML = title + "<br /><input id='new_entry' size='30'><button onclick='addEntry()'>Add entry</button><br /><div id='entries'></div>";
+      if(entries)
       {
-        document.getElementById('entries').innerHTML = i;
+        for(var i = 0; i < entries.length; i++)
+        {
+          document.getElementById('entries').innerHTML = i;
+        }
       }
     }
   }
-}
 
-function init()
-{
-  if(wave && wave.isInWaveContainer())
+  function init()
   {
-    wave.setStateCallback(render);
-    wave.setParticipantCallback(render);
+    if(wave && wave.isInWaveContainer())
+    {
+      wave.setStateCallback(render);
+      wave.setParticipantCallback(render);
+    }
   }
-}
-gadgets.util.registerOnLoadHandler(init);
+  gadgets.util.registerOnLoadHandler(init);
+});
